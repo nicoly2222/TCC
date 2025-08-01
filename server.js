c8b0bb73d7510faafbf09b200bb73efe03e36c7d
nique <hjihi568@gmail.com>
2025-08-01 15:38:13 -0300

    tcc
    O site VitalSync foi criado para melhorar a organização de  gestão de contratos, oferecendo aos administradores uma plataforma online com cadastro, gerenciamento e alertas de vencimentos de contratos. O acesso é feito via-web, mediante cadastro no sistema, permitindo aos trabalhadores a acompanhar atualizações e informações de contratos de forma prática e eficiente.

---
+++ server.js
@@ -0,0 +1,190 @@
+/**
+* @fileoverview Defines routes, paths and nodejs server configs with expressjs.
+* @project Contracts Manager - https://contracts-manager.onrender.com
+* @author Obrymec - obrymecsprinces@gmail.com
+* @created 2022-01-30
+* @updated 2024-05-02
+* @supported DESKTOP
+* @file server.js
+* @version 0.0.2
+*/
+
+// Plugin dependencies.
+const parser = require ("body-parser");
+const express = require ("express");
+const app = express ();
+const port = 5300;
+
+// Custom dependencies.
+const api = require ("./back_end/api.js");
+const root = `${__dirname}/front_end`;
+
+// App configurations.
+app.use (parser.urlencoded ({extended: true}));
+app.use (express.static (root));
+app.use (parser.json ());
+
+// Root route.
+app.get ('/', (_, res) => (
+  res.sendFile (
+    "index.html", {
+      root: `${root}/public`
+    }
+  )
+));
+// Available employees.
+app.get (
+  "/employees-availables",
+  (_, res) => (
+    api.load_availables_employees (
+      null, result => (
+        res.send (result)
+      )
+    )
+  )
+);
+// Running contracts.
+app.get (
+  "/running-contracts",
+  (_, res) => (
+    api.load_running_contracts (
+      null, result => (
+        res.send (result)
+      )
+    )
+  )
+);
+// Expired contracts.
+app.get (
+  "/expired-contracts",
+  (_, res) => (
+    api.load_expired_contracts (
+      null, result => (
+        res.send (result)
+      )
+    )
+  )
+);
+// Adds employee.
+app.post (
+  "/add-employee",
+  (req, res) => (
+    api.add_employee (
+      req.body.data,
+      result => (
+        res.send (result)
+      )
+    )
+  )
+);
+// Adds contract.
+app.post (
+  "/add-contract",
+  (req, res) => (
+    api.add_contract (
+      req.body.data,
+      result => (
+        res.send (result)
+      )
+    )
+  )
+);
+// Overrides contract.
+app.post (
+  "/override-contract",
+  (req, res) => (
+    api.override_contract (
+      req.body.data,
+      result => (
+        res.send (result)
+      )
+    )
+  )
+);
+// Removes contract.
+app.post (
+  "/remove-contract",
+  (req, res) => (
+    api.remove_contract (
+      req.body,
+      result => (
+        res.send (result)
+      )
+    )
+  )
+);
+// Adds mistake.
+app.post (
+  "/add-mistake",
+  (req, res) => (
+    api.add_mistake (
+      req.body.data,
+      result => (
+        res.send (result)
+      )
+    )
+  )
+);
+// Available faults.
+app.post (
+  "/mistakes-availables",
+  (req, res) => (
+    api.load_mistakes (
+      req.body,
+      result => (
+        res.send (result)
+      )
+    )
+  )
+);
+// Sign up.
+app.post (
+  "/sign-up",
+  (req, res) => (
+    api.sign_up (
+      req.body.data,
+      result => (
+        res.send (result)
+      )
+    )
+  )
+);
+// Sign in.
+app.post (
+  "/sign-in",
+  (req, res) => (
+    api.sign_in (
+      req.body.data,
+      result => (
+        res.send (result)
+      )
+    )
+  )
+);
+// Mail sender.
+app.post (
+  "/expired-contracts-data",
+  (req, res) => (
+    api.send_gmail (
+      req.body,
+      result => (
+        res.send (result)
+      )
+    )
+  )
+);
+
+// Starts the server.
+app.listen (port, err => {
+	// Whether an error is thrown.
+	if (err) {
+		// Displays this error message.
+		console.error ("Server Error: ", err);
+	// Otherwise.
+	} else {
+		// Makes a warn about server starting.
+		console.log (
+			"Server started at port: ", port
+		);
+	}
+});
